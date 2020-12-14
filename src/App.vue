2d343e2ab914c8e6b8068e55493b1d7f3d406268
hasshi <hashimoto7965@gmail.com>
2020-12-15 03:07:12 +0900

    add refrersh button

--- src/App.vue
+++ src/App.vue
@@ -2,49 +2,28 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="orange"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h2>eNNergy Drink </h2>
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
     <v-main>
       <RandomGet/>
     </v-main>
+
+  <v-footer>
+    <div class="flex-grow-1"></div>
+    <div> @Hasshi7965 </div>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
 import RandomGet from './components/randomget';
+// import RandomGet from './components/HelloWorld';
 
 export default {
   name: 'App',
--- src/components/randomget.vue
+++ src/components/randomget.vue
@@ -1,37 +1,33 @@
 <template>
-  <v-app>
-    <v-layout wrap>
-        
-    <v-row>
-        <v-col
-        v-for="n in 12"
-        :key="n"
-        class="d-flex child-flex"
-        cols="3"
-        >
-        <v-img
-            :src="items[n-1]"
-            aspect-ratio="1"
-            class="grey lighten-2"
-        >
-            <template v-slot:placeholder>
-            <v-row
-                class="fill-height ma-0"
-                align="center"
-                justify="center"
-            >
-                <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-                ></v-progress-circular>
-            </v-row>
-            </template>
-        </v-img>
-        </v-col>
-    </v-row>
-    <v-flex xs12 sm6 md12 text-center my-5><v-btn>押して</v-btn></v-flex>
-    </v-layout>
-  </v-app>
+    <v-container>
+        <h2 class="orange--text font-weight-bold  mb-3">
+          Select your energy
+        </h2>
+        <v-row v-for="(r, keyr) in rowCount" :key="keyr" >
+            <v-col v-for="(c, keyc) in colNumber" :key="keyc">
+                <v-img
+                    :src="items[keyr*colNumber+keyc]"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                    height="pixshape"
+                    width="pixshape"
+                >
+                    <template v-slot:placeholder>
+                    <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                    >
+                        <v-progress-circular indeterminate color="grey lighten-5" />
+                    </v-row>
+                    </template>
+                </v-img>
+            </v-col>
+        </v-row>
+        <v-flex xs12 sm6 md12 text-center my-5><v-btn
+                        @click="get_gifs"
+                    color="orange">refresh</v-btn></v-flex>
+    </v-container>
 </template>
 
 
@@ -51,7 +47,7 @@
         this.axios.get("https://c7c273c3f0db.ngrok.io/wakeup_test"
         ).then(() => {
             this.items = []
-            for (let step = 0; step < 16; step++){
+            for (let step = 0; step < this.colNumber*this.rowCount; step++){
                 shasum.update((new Date).toString());
                 //this.axios.get("http://localhost:15000/generate_gif", 
                 this.axios.get("https://c7c273c3f0db.ngrok.io/generate_gif?hash=" + String(shasum.digest('hex')), 
@@ -63,7 +59,6 @@
                     //this.items.push(prefix + response.data.img);
                     this.dataUrl = prefix + base64;
                     this.items.push(prefix + base64);
-                    console.log(this.dataUrl);
                 })
                 .catch((e) => {
                     alert(e);
@@ -89,6 +84,29 @@
 
         });
     },
+
+    computed: {
+      colNumber: function() {
+        let number;
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs': number = 3; break;
+          case 'sm': number = 4; break;
+          case 'md': number = 5; break;
+          case 'lg': number = 6; break;
+          case 'xl': number = 7; break;
+        }
+        return number;
+      },
+
+      pixshape: function() {
+        return Math.ceil(this.$vuetify.breakpoint.width / this.colNumber);
+      },
+
+      rowCount:function(){
+        return Math.ceil((this.$vuetify.breakpoint.height - 260)  / this.pixshape);
+      },
+    },
+
     methods: {
       getIp: function() {
         this.items = []
@@ -99,7 +117,6 @@
                 const prefix = `data:image/png;base64,`;
                 //const base64 = new Buffer(response.data.img, "binary").toString("base64");
                 for (let ret_img in response.data.img) {
-                    console.log(response.data.img[ret_img]);
                     this.dataUrl = prefix + response.data.img[ret_img];
                     this.items.push(prefix + response.data.img[ret_img]);
                 }
@@ -112,29 +129,35 @@
             });
       },
 
-      get_gifs: function() {
-        this.items = []
-        // const params = new URLSearchParams();
-        this.axios.get('https://c970cc9ad36a.ngrok.io/generate_gif')
-            .then((response) => {
-                const prefix = `data:image/png;base64,`;
-                //const base64 = new Buffer(response.data.img, "binary").toString("base64");
-                for (let ret_img in response.data.img) {
-                    console.log(response.data.img[ret_img]);
-                    this.dataUrl = prefix + response.data.img[ret_img];
-                    this.items.push(prefix + response.data.img[ret_img]);
-                }
-
-                console.log(response.data.img);
-                //console.log(response.data.img);
-            })
-            .catch((e) => {
-                alert(e);
-            });
+      get_gifs:function() {
+        this.axios.get("https://c7c273c3f0db.ngrok.io/wakeup_test"
+        ).then(() => {
+            this.items = []
+            for (let step = 0; step < this.colNumber*this.rowCount; step++){
+                shasum.update((new Date).toString());
+                //this.axios.get("http://localhost:15000/generate_gif", 
+                this.axios.get("https://c7c273c3f0db.ngrok.io/generate_gif?hash=" + String(shasum.digest('hex')), 
+                {responseType: "arraybuffer"})
+                .then((response) => {
+                    const prefix = `data:${response.headers["content-type"]};base64,`;
+                    const base64 = new Buffer(response.data, "binary").toString("base64");
+                    //this.dataUrl = prefix + response.data.img;
+                    //this.items.push(prefix + response.data.img);
+                    this.dataUrl = prefix + base64;
+                    this.items.push(prefix + base64);
+                    console.log(this.dataUrl);
+                })
+                .catch((e) => {
+                    alert(e);
+                });
         }
-
-
-
+        }).catch(() => {
+            alert("back end server is off-line \n no refresh");
+        });
+      },
+      itemCountInRow:function(row){
+        return this.attendances.slice((row - 1) * this.colNumber, row * this.colNumber)
+      }
     }
   }
 </script>
